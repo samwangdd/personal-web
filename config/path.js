cc1367d1385718baed75ce45b26f092c4d73565e
sam <852530453@qq.com>
2019-04-04 18:17:23 +0800

    配置文件，如node

---
+++ config/path.js
@@ -0,0 +1,14 @@
+/**
+ * 管理项目文件路径
+ */
+
+const path = require('path');
+const fs = require('fs');
+const url = require('url');
+
+const appDirectory = fs.realpathSync(process.cwd());
+console.log('appDirectory :', appDirectory);
+
+module.exports = {
+  htmlTemplate: appDirectory
+}
