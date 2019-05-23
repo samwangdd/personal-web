4a0c77611ec6a8d08cb651c30d5f3095a9c9afb4
🐥 wangs <wangs@chinawyny.com>
2019-05-23 14:54:42 +0800

    [chore] add .prettierrc

---
+++ src/components/RenderProps/MouseTraker.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MouseTraker extends Component {
+  render() {
+    const po = this.props.mouse
+    return (
+      <div>
+        <p>当前鼠标位置为：（{po.x}，{po.y}）</p>
+      </div>
+    );
+  }
+}
+
+export default MouseTraker;
