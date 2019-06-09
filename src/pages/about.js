7b2c28bb55baf93878540363ea09b3b983bcb635
Sam <mac@Sams-MBP.local>
2019-06-09 16:20:21 +0800

    [chore] add eslint plugin && [feat] useReducer

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,20 +5,21 @@ import Mouse from '@components/RenderProps/Mouse';
 import MouseTraker from '@components/RenderProps/MouseTraker';
 
 class about extends Component {
-  renderCat = (mouse) => {
+  renderCat = mouse => {
     console.log('renderCat :');
-    return <MouseTraker mouse={mouse} />
-  }
-  render () {
+    return <MouseTraker mouse={mouse} />;
+  };
+  render() {
     console.log('about :');
     return (
-      <div style={{ height: "1000px" }}>
+      <div style={{ height: '1000px' }}>
         about me
         <h1>移动鼠标！</h1>
-        <Mouse render={(mouse) => {
-          console.log('linerender :');
-          return <MouseTraker mouse={mouse} />
-        }}
+        <Mouse
+          render={mouse => {
+            console.log('linerender :');
+            return <MouseTraker mouse={mouse} />;
+          }}
         />
       </div>
     );
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,44 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer } from 'react';
 
-function useDocumentTitle (title) {
-  // 类似 componentDidMount 和 componentDidUpdate
-  useEffect(
-    () => {
-      document.title = title
-      return () => (document.title = '前端精读')
-    },
-    [title]
+const initialState = { count: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'incremet':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    case 'reset':
+      return { count: 0 };
+    default:
+      throw new Error();
+  }
+}
+// hooks写法
+function Home() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div>
+      <p>you clicked {state.count} times</p>
+      <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
+      <button onClick={() => dispatch({ type: 'incremet' })}>+</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+    </div>
   );
 }
 
-// hooks写法
-function Home () {
-  const [count, setCount] = useState(0);
-  useDocumentTitle(`you clicked ${count} times`);
-  return (
-    <div>
-      <p>you clicked {count}</p>
-      <button onClick={() => setCount(count + 1)}>Click me!</button>
-    </div>
-  )
-}
-
 export default Home;
-
-// 类组件写法
-/* class test extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0
-    }
-  }
-  render() {
-    return (
-      <div>
-        <p>you clicked {this.state.count}</p>
-        <button onClick={() => this.setState({count: this.state.count + 1})}>Click me!</button>
-      </div>
-    );
-  }
-} */
