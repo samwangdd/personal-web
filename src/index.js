a04fc1a9e90132dff843cb0e5563841708317fb3
Sam <wyysammore@gmail.com>
2020-04-14 11:24:39 +0800

    feat: support sentry

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import * as Sentry from '@sentry/browser';
 import 'semantic-ui-css/semantic.min.css';
 
 import Home from '@pages/home.tsx';
 import About from '@pages/about';
 import Content from '@pages/content.tsx';
 import TopMenu from '@components/TopMenu';
+
+Sentry.init({ dsn: 'https://1b59d3a0ad894cef98f02a371420191d@o377378.ingest.sentry.io/5199487' });
 
 const MenuList = [
   {
